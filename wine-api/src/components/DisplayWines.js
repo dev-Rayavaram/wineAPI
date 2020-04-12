3038836abb11d259fcba00b6b79b89fe7ab473fe
Student_M20 <student_m20@PS-DAL-M20.local>
2020-04-12 13:31:46 -0500

    added get wine detail for single id

--- wine-api/src/components/DisplayWines.js
+++ wine-api/src/components/DisplayWines.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-
+import axios from 'axios'
 const DisplayWines=(props) =>{
   console.log("inside DisplayWines");
   if(typeof props.location.params  !== "undefined")
@@ -15,7 +15,7 @@ const DisplayWines=(props) =>{
                   <th>ID</th><th>Name</th><th>Year</th><th>Grapes</th><th>Country</th>
                 </tr>
 
-        {
+              {
                 props.location.params.data.map((item,index)=>{return (
                 <Wine key={index} value={item} />
                 )}) 
@@ -41,17 +41,84 @@ const DisplayWines=(props) =>{
 }
 
 export default DisplayWines;
+const SingleWine=(props)=>{
+  return(
+    <>
+    console.log("inside SingleWine")
+    console.log(props)
+    </>
+  )
+}
 
 class Wine extends Component{
-  render(){
-    return(
-      <React.Fragment>
-        <tr>
-          <td>{this.props.value.id}  </td><td> {this.props.value.name} </td> <td> {this.props.value.year} </td> <td>{this.props.value.grapes} </td> <td> {this.props.value.country}</td>
+  constructor(props){
+    super(props);
+    this.state={
+      wineData:[],
+      buttonClicked:false
+    }
+    this.getSingleWine = this.getSingleWine.bind(this);
+  }
+   async getSingleWine(e){
+     let id=e.target.innerHTML;
+     console.log("inside getSingleWine",)
+     let url = `http://myapi-profstream.herokuapp.com/api/61fcf4/wines/${id}`
+     try{
+            axios.headers={
+          
+                  'Access-Control-Allow-Origin': '*',
+                  'Content-Type': 'application/json',
+            } 
+            const results = await axios.get(url,{params:{ 
+              'id':id
+            }});
+           console.log("inside get single wine info");
+           console.log(results);
+           this.setState({wineData:results});
+           this.setState({buttonClicked:true})
+     }
+     catch (e){
+       console.log(e);
+     }
+     e.preventDefault();
 
-        </tr>
-      </React.Fragment>
-    )
+  }
+  
+  render(){
+    if(this.state.buttonClicked===true){
+      let localData = this.state.wineData.data;
+        console.log("localData",localData)
+      return(
+        <React.Fragment>
+            <h2>Wine details are</h2>
+            <img src= {localData.picture} width='150px' height='150px' alt={localData.id}/>
+            <h3>Name: {localData.name}</h3>
+            <h3>Year: {localData.year}</h3>
+            <h3>Grapes: {localData.grapes}</h3>
+            <h3>Country: {localData.country}</h3>
+            <h3>Region: {localData.region}</h3>
+            <p>Description: {localData.description}</p>
+            <h3>Price: {localData.price}</h3>
+            <h3>instance_id: {localData.instance_id}</h3>
+            <h3>created_at: {localData.created_at}</h3>
+            <h3>updated_at: {localData.updated_at}</h3>
+         </React.Fragment>
+       )
+    
+    }
+    else{
+      return(
+        <React.Fragment>    
+          <tr>
+            <td>
+              <button  onClick={this.getSingleWine}>{this.props.value.id}           
+              </button>         
+             </td><td> {this.props.value.name} </td> <td> {this.props.value.year} </td> <td>{this.props.value.grapes} </td> <td> {this.props.value.country}</td>
+          </tr>
+        </React.Fragment>
+       )
+    }
+  
   }
   
 }
